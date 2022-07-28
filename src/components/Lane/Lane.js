c511fe9dbf9969a637a4148856bb0709bb181433
Sergio Valadez <sergioval@gmail.com>
2022-07-27 19:21:38 -0500

    Initial commit

---
+++ src/components/Lane/Lane.js
@@ -0,0 +1,26 @@
+import Task from '../Task/Task';
+import './Lane.css';
+function Lane({ laneId, title, loading, error, tasks, onDragStart, onDragOver, onDrop }) {
+  return (
+    <div className='Lane-wrapper'
+    onDragOver={onDragOver}
+    onDrop={(e) => onDrop(e, laneId)}
+    >
+      <h2>{title}</h2>
+      {loading || error ? (
+        <span>{error || 'Loading...'}</span>
+      ) : (
+        tasks.map((task) => (
+           <Task
+             key={task.id}
+             id={task.id}
+             title={task.title}
+             body={task.body}
+             onDragStart={onDragStart}
+           />
+        ))
+      )}
+    </div>
+  );
+}
+export default Lane;
